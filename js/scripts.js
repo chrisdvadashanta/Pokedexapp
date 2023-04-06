80e9542bfd5f2cf5ff940b038f65d2a68c26babe
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-04-06 15:22:42 +0200

    changed the empty array of pokemon list and the definition of types

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,26 @@
-let pokemonList = {}
-let pokemon1 = {
-    name:Charizard,
+let pokemonList = [
+{
+    name:'Charizard',
     hight:1.7,
     weight:90.5,
-    types:'fire',
-}
-let pokemon2 = {
-    name:Hypno,
+    types:['fire','flying']
+},
+{   
+    name:'Hypno',
     hight:1.6,
     weight:75.6,
-    types:'psychic',
-}
-let pokemon3 = {
-    name:Chansey,
+    types:['psychic'],
+},
+{
+   name:'Chansey',
     hight:1.1,
     weight:34.6,
-    types:'normal',
-}
-let pokemon4 = {
-    name:Pichu,
+    types:['normal'],
+},
+{
+    name:'Pichu',
     hight:0.3,
     weight:2,
-    types:'electric',
-}
+    types:['electric'],
+},
+];
