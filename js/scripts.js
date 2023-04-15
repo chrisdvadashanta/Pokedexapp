fe33416846731f191fecdfbf4b084cd42ba6ce5b
chrisdvadashanta <127535781+chrisdvadashanta@users.noreply.github.com>
2023-04-15 20:26:17 +0200

    new function got added to add pokemons

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,52 @@
 let pokemonList = [
-{
-    name:'Charizard',
-    hight:1.7,
-    weight:90.5,
-    types:['fire','flying']
-},
-{   
-    name:'Hypno',
-    hight:1.6,
-    weight:75.6,
-    types:['psychic'],
-},
-{
-    name:'Chansey',
-    hight:1.1,
-    weight:34.6,
-    types:['normal'],
-},
-{
-    name:'Pichu',
-    hight:0.3,
-    weight:2,
-    types:['electric'],
-},
+    {
+        name: 'Charizard',
+        hight: 1.7,
+        weight: 90.5,
+        types: ['fire', 'flying']
+    },
+    {
+        name: 'Hypno',
+        hight: 1.6,
+        weight: 75.6,
+        types: ['psychic'],
+    },
+    {
+        name: 'Chansey',
+        hight: 1.1,
+        weight: 34.6,
+        types: ['normal'],
+    },
+    {
+        name: 'Pichu',
+        hight: 0.3,
+        weight: 2,
+        types: ['electric'],
+    },
 ];
 
-//Loop to display Pokemon and their hight
-for (let i=0 ; i < pokemonList.length ; i++ ){
-    if (pokemonList[i].hight > 1.6){
-        document.write( '<p>' + pokemonList[i].name+ ' : '+ '<p>'+ 'hight: ' + pokemonList[i].hight +' - ' +  'Wow, that\'s big!')
-    } 
-    else {
-        document.write('<p>' + pokemonList[i].name+ ' : ' + '<p>'+ 'hight: ' +  pokemonList[i].hight)
+//forEach to display Pokemon and their hight
+pokemonList.forEach(function (pokemonList) {
+    if (pokemonList.hight > 1.6) {
+        document.write('<p>' + pokemonList.name + ' : ' + 'hight - ' + pokemonList.hight + ' - ' + 'Wow, that\'s big!')
     }
-}
+    else {
+        document.write('<p>' + pokemonList.name + ' : ' + 'hight -' + pokemonList.hight)
+    }
+})
+
+let pokemonRepository = (function () {
+    let pokemonList = []
+    function add(pokemon) {
+        pokemonList.push(pokemon)
+    };
+    function getall() {
+        return pokemonList
+    };
+    return {
+        add: add,
+        getall: getall,
+    };
+
+})();
+
